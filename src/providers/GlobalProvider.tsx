19092466c6af1b2cd51a74ecd767692a62f998a9
KimYoungWoong <highero700@gmail.com>
2026-04-25 14:50:03 +0900

    add: tanstack query, zustand

--- src/providers/GlobalProvider.tsx
+++ src/providers/GlobalProvider.tsx
@@ -1,7 +1,15 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import queryClient from '@/lib/queryClient';
 import { EnhancedToastProvider } from '@/ui-lib/components/toast';
 
 const GlobalProvider = ({ children }: { children: React.ReactNode }) => {
-  return <EnhancedToastProvider>{children}</EnhancedToastProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <EnhancedToastProvider>{children}</EnhancedToastProvider>
+      {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+    </QueryClientProvider>
+  );
 };
 
 export default GlobalProvider;
